07988e39f634b07599c110471c63cb0c20228cea
Jack Maney <jackmaney@gmail.com>
2020-05-13 10:02:09 -0500

    Code formatting

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
-    if ($("#resume-buttons").length){
+    if ($("#resume-buttons").length) {
         // Hackety hackety hack hack hack...
         var pathname = $(location).attr("pathname");
 
-        if (!pathname.startsWith("/resume") || pathname.match(/resume\/printable\/?$/)){
+        if (!pathname.startsWith("/resume") || pathname.match(/resume\/printable\/?$/)) {
             $("#resume-buttons").empty();
             $("#resume-buttons").remove();
         }
