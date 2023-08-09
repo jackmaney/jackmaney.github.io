cd32f7ef3217d3b459e7caae45a5ed04727eae47
Jack Maney <jackmaney@gmail.com>
2023-08-09 09:50:55 -0500

    Redirecting / to /resume/

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
+
+    // Redirect from the main page to the resume page
+    var pathname = $(location).attr("pathname");
+    if (pathname == "/") {
+        window.location.replace("/resume/");
+    }
+
+    // Add hoverable tooltips for resume buttons
     $('[data-toggle="tooltip"]').tooltip();
 
+    // Make sure the PDF and "printable version" resume buttons only show up where they're supposed to.
     if ($("#resume-buttons").length) {
         // Hackety hackety hack hack hack...
-        var pathname = $(location).attr("pathname");
-
+        
         if (!pathname.startsWith("/resume") || pathname.match(/resume\/printable\/?$/)) {
             $("#resume-buttons").empty();
             $("#resume-buttons").remove();
